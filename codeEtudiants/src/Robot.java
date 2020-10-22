329af91c5f6d8f5248e9ab193f6ad728566a9074
Benjamin Cathelineau <cathelib@ensipc279.ensimag.fr>
2020-10-22 14:23:54 +0200

    robot

---
+++ codeEtudiants/src/Robot.java
@@ -0,0 +1,18 @@
+
+public abstract class Robot {
+	void RemplirReservoir() {
+		
+	}
+	Case getPosition() {
+		
+	}
+	void setPosition(Case localisation) {
+		
+	}
+	double getVitesse(NatureTerrain) {
+		
+	}
+	void deverserEau(int vol) {
+		
+	}
+}
