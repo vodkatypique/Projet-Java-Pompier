576ababbb6749e34a1c8468c3e247aa3b8c34033
Vodkatypique <clement.caffin@gmail.com>
2020-10-22 15:15:57 +0200

    Carte voisin

--- codeEtudiants/src/Carte.java
+++ codeEtudiants/src/Carte.java
@@ -11,6 +11,11 @@ public class Carte {
         this.nbColonnes = nbColonnes;
         this.tailleCases = 1;
         this.plateau = new Case[nbLignes][nbColonnes];
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                this.plateau[i][j] = new Case(i, j, NatureTerrain.FORET);
+            }
+        }
     }
 
     public int getTailleCases() {
@@ -27,5 +32,21 @@ public class Carte {
 
     public Case getCase(int lig, int col) {
         return plateau[lig][col];
+    }
+
+    public Case getVoisin(Case src, Direction dir) {
+        if (this.voisinExiste(src, dir)) {
+            return this.plateau[src.getLigne()][src.getColonne()];
+        }
+        return null; //TODO ERRRREEEEEUUUUR
+    }
+
+    public boolean voisinExiste(Case src, Direction dir) {
+        if ((src.getColonne() == 0 && dir == Direction.OUEST) || (src.getColonne() == this.nbColonnes && dir == Direction.EST) || (src.getLigne() == 0 && dir == Direction.NORD) || (src.getLigne() == this.nbLignes && dir == Direction.SUD)) {
+            return false;
+        }
+        return true;
+
+
     }
 }
